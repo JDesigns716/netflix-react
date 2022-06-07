37149790790a270a6d206a2ec5163f3b79be920e
JDesigns716 <91083291+JDesigns716@users.noreply.github.com>
2022-06-07 13:29:06 -0400

    added custom hooks

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { useContent } from '../hooks'
 
 export default function Browse() {
+  // we need the series and films
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+  // we need slides
+  // pass iyt into browse container
+
   return <p>Hello from browse!</p>
 }
